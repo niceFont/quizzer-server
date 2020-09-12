fe1d4b9914bc732c108ec4a7b4b6d245f2b7c6ab
niceFont <peterwilms@hotmail.de>
2020-09-12 13:17:34 +0200

    maybe now

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/quizzer-server
-NODE_ENV=production node index.js /dev/null 2> /dev/null < /dev/null &
-exit 1 
+NODE_ENV=production node index.js > app.out.log 2> app.err.log < /dev/null & 
