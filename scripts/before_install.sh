16908974ba94ae31cfa11f468e62fc60e486665d
niceFont <peterwilms@hotmail.de>
2020-09-12 18:50:15 +0200

    next try

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pkill node > /dev/null 2>&1
+rm -rf /home/ubuntu/quizzer-server
 exit 0
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 cd /home/ubuntu/quizzer-server
-git reset --hard
-git pull
-rm -rf node_modules
 npm install
 cp /home/ubuntu/production.json /home/ubuntu/quizzer-server/config
